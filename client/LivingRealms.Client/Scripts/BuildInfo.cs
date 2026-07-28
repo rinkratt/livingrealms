d38bd5611fbbb63c0753ac9db4ccd122fafcf8e5
rinkratt <rinkratt@users.noreply.github.com>
2026-07-28 02:22:13 -0500

    Publish synchronized 0.9.20 release

--- client/LivingRealms.Client/Scripts/BuildInfo.cs
+++ client/LivingRealms.Client/Scripts/BuildInfo.cs
@@ -2,7 +2,7 @@ namespace LivingRealms.Client;
 
 public static class BuildInfo
 {
-    public const string Version = "0.9.19";
-    public const string Name = "WORLD-SCALE ENVIRONMENT";
+    public const string Version = "0.9.20";
+    public const string Name = "SUSTAINABLE SETTLEMENTS";
     public const string DisplayLabel = $"BUILD {Version} - {Name}";
 }
